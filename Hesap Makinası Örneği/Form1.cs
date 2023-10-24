ca3e01ebe4fc53f7dfae353d6a183fec7cd0e31f
ofaru <ofaru@MSI>
2023-10-24 23:45:30 +0300

    Proje dosyası ekle.

---
+++ Hesap Makinası Örneği/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hesap_Makinası_Örneği
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayı1;
+            int sayı2;
+            int toplam;
+
+
+            sayı1 = Convert.ToInt32(textBox1.Text);
+            sayı2 = Convert.ToInt32(textBox2.Text);
+
+            toplam = sayı1 + sayı2;
+
+
+            label4.Text = toplam.ToString();
+            
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a, b, sonuc;
+
+            a = Convert.ToInt32(textBox3.Text);
+            b = Convert.ToInt32(textBox4.Text);
+
+            sonuc = a * b;
+
+            label8.Text = sonuc.ToString();
+
+        }
+    }
+}
